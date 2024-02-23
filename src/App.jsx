4e0d5144add405e6d48f3a87bb2bfb345345961c
Benjamin Huy Pham <benjaminhpham@gmail.com>
2024-02-23 14:56:31 -0700

    stateful components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import Youtube from "./Youtube";
+import { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
   return (
     <>
       <h1>Classic React App</h1>
-      <Youtube />
+      <h2>Counter: {counter}</h2>
+      <button onClick={addValue}>Add</button>
+      <button onClick={removeValue}>Remove</button>
+      <p>footer: {counter}</p>
     </>
   );
 }
