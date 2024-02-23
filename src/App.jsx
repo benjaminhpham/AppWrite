d9d5ded9ac57489536fc208138346a4abe44b452
Benjamin Huy Pham <benjaminhpham@gmail.com>
2024-02-23 13:14:39 -0700

    custom react

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,10 @@
-// Topics to learn
-// -> Core of React (State of UI manipulation, DOM)
-// -> component reusability
-// -> reusing of components (props)
-// -> how to progate change (hooks)
+import Youtube from "./Youtube";
 
-// Additional addon to React
-// -> router (React don't have router)
-// -> state management (React don't have state management: redux, redux toolkit, zustard, context API)
-// -> class based component (legacy code)
-// -> BAAS Apps (social media clone, e-commerce app...)
-
-// Alter React
-// -> React is not a complete solution in most cases
-//       -> no SEO, browser render of JS, no routing
-// -> framework: NextJS, Gatsby, Remix
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Classic React App</h1>
+      <Youtube />
     </>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit Google",
+};
+
+const MyApp = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const areactElement = React.createElement(
+  "a",
+  { href: "https://www.google.com", target: "_blank" },
+  "Click to visit Google"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
